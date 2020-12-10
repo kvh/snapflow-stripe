af4a5ad9c0f64deeec95e7d4f2abc78895cc5d9c
Ken Van Haren <kenvanharen@gmail.com>
2020-12-10 12:17:28 -0800

    Git workflow and tests

--- tests/test_stripe.py
+++ tests/test_stripe.py
@@ -1,13 +1,21 @@
 from pprint import pprint
 
+import pytest
 from snapflow import Environment
 from snapflow.core.graph import Graph
 from snapflow.testing.utils import get_tmp_sqlite_db_url
 
+TEST_API_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 
-def test_stripe(api_key: str):
+@pytest.mark.parametrize("api_key", [(TEST_API_KEY)])
+def test_stripe(api_key):
+    run_stripe_test(api_key)
+
+def run_stripe_test(api_key: str):
     import snapflow_stripe
 
+    if not api_key:
+        api_key = TEST_API_KEY
     env = Environment(metadata_storage="sqlite://")
     g = Graph(env)
     s = env.add_storage(get_tmp_sqlite_db_url())
@@ -19,15 +27,12 @@ def test_stripe(api_key: str):
         config={"api_key": api_key},
     )
     clean_charges = g.create_node("stripe.clean_charges", upstream=raw_charges)
-    output = env.produce(clean_charges, g, target_storage=s, node_timelimit_seconds=5)
+    output = env.produce(clean_charges, g, target_storage=s, node_timelimit_seconds=.01)
     records = output.as_records_list()
-    assert len(records) > 100
+    assert len(records) >= 100
     assert records[0]["amount"] == 100
 
 
 if __name__ == "__main__":
-    test_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
     api_key = input("Enter Stripe API key (default test key): ")
-    if not api_key:
-        api_key = test_key
     test_stripe(api_key)
