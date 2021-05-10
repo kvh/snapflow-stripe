cbadf84ec011ff71424157a37e41f16483e0cb0b
Ken Van Haren <kenvanharen@gmail.com>
2021-05-10 08:51:08 -0700

    Subscriptions importer

--- snapflow_stripe/__init__.py
+++ snapflow_stripe/__init__.py
@@ -2,6 +2,7 @@ from typing import TypeVar
 
 from snapflow import SnapflowModule
 
+from .functions.import_subscriptions import import_subscriptions
 from .functions.clean_charges import clean_charges
 from .functions.import_charges import import_charges
 from .functions.import_refunds import import_refunds
@@ -10,8 +11,10 @@ from .functions.import_refunds import import_refunds
 StripeCharge = TypeVar("StripeCharge")
 StripeChargeRaw = TypeVar("StripeChargeRaw")
 StripeRefundRaw = TypeVar("StripeRefundRaw")
+StripeSubscriptionRaw = TypeVar("StripeSubscriptionRaw")
 
 module = SnapflowModule("stripe", py_module_path=__file__, py_module_name=__name__,)
 module.add_function(clean_charges)
 module.add_function(import_charges)
 module.add_function(import_refunds)
+module.add_function(import_subscriptions)
